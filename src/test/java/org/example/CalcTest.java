a83c15be58aa9e885602594f7bed64af3d23da91
BillChen817 <137595418+BillChen817@users.noreply.github.com>
2023-08-16 23:59:12 +1200

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,15 +1,5 @@
 package org.example;
 
-public class Calc {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-}
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
@@ -18,12 +8,12 @@ public class CalcTest {
   Calc c = new Calc();
 
     @Test
-    void testAddition() {
+    void testAddition1() {
     assertEquals(4，c.add(2,2));
     }
 
     @Test
-    void testAddition() {
+    void testAddition2() {
         assertEquals(4, c.add(2,2));
     }
 
