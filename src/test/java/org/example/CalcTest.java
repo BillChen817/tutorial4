c64920040b1ff03d93586914635c0d74df60ef92
BillChen817 <137595418+BillChen817@users.noreply.github.com>
2023-08-16 23:48:10 +1200

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,5 +1,16 @@
 package org.example;
 
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
