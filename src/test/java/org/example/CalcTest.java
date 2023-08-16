87ade6d1ad8df406133e61aaa38b5b6d333f6b52
BillChen817 <137595418+BillChen817@users.noreply.github.com>
2023-08-17 00:01:57 +1200

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,16 +1,11 @@
 package org.example;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
   Calc c = new Calc();
 
-    @Test
-    void testAddition1() {
-    assertEquals(4，c.add(2,2));
-    }
+   
 
     @Test
     void testAddition2() {
